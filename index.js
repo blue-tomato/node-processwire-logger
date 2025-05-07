d55b6f84742049a57df6c4f12b0efbb8a47153b5
Markus Tiefenbacher <markus.tiefenbacher@gmail.com>
2025-05-07 11:11:35 +0000

    Optimize to nodejs 22

--- index.js
+++ index.js
@@ -1,36 +1,49 @@
-"use strict";
+import { createWriteStream, mkdirSync, existsSync } from 'node:fs';
+import { join } from 'node:path';
 
-const fs = require('fs');
-const format = require('date-fns/format');
+export default class Logger {
+  #stream;
 
-class Logger {
-
-  constructor(logFileName, assetsRootPath) {
-    this.openFileStream(logFileName, assetsRootPath);
+  /**
+   * @param {string} logFileName - Name der Log-Datei (ohne Pfad)
+   * @param {string} [assetsRootPath=process.cwd()] - Root-Verzeichnis für Logs
+   */
+  constructor(logFileName, assetsRootPath = process.cwd()) {
+    const logsDir = join(assetsRootPath, 'logs');
+    if (!existsSync(logsDir)) {
+      mkdirSync(logsDir, { recursive: true });
+    }
+    this.#stream = createWriteStream(
+      join(logsDir, `${logFileName.toLowerCase()}.txt`),
+      { flags: 'a' }
+    );
   }
 
-  openFileStream(logFileName, assetsRootPath) {
-    this.fileStream = fs.createWriteStream(`${assetsRootPath}logs/${logFileName.toLowerCase()}.txt`, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    });
+  /**
+   * Schreibt eine Zeile ins Log
+   * @param {string} [userName='nodejs']
+   * @param {string} [url='?']
+   * @param {string} [msg='-']
+   */
+  log(userName = 'nodejs', url = '?', msg = '-') {
+    const timestamp = new Intl.DateTimeFormat('en-CA', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hourCycle: 'h23'
+    }).format(new Date()).replace('T', ' ');
+
+    const line = `${timestamp}\t${userName}\t${url}\t${msg}\n`;
+    this.#stream.write(line);
   }
 
-  write(userName = 'nodejs', url = '?',  msg = '-', callback = function() {}) {
-    let data = [
-      format(new Date(), 'YYYY-MM-DD HH:mm:ss'),
-      userName,
-      url,
-      msg
-    ];
-    data = data.join("\t") + '\n';
-    this.fileStream.write(data);
-    callback();
-  }
-
+  /**
+   * Schliesst den Stream
+   */
   close() {
-    this.fileStream.end();
+    this.#stream.end();
   }
-
 }
-
-module.exports = Logger;
